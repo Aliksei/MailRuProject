48e209f186a3bfbbe92d9e7530745a7ff0a53b2d
Aliksei <kikit33@hotmail.com>
2015-04-14 17:22:20 +0300

    yandex elements,listener

--- src/main/java/DriverUtils.java
+++
@@ -1,9 +0,0 @@
-import org.openqa.selenium.By;
-
-public class DriverUtils {
-
-    public static boolean isElementExists(By locator){
-
-        return MozillaBrowser.getInstacne().findElements(locator).size()>0;
-    }
-}
